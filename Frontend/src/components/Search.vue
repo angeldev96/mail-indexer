b4d1398e41731182441ac1a8c17baa5b38ff4abd
angelde96 <arivalladares2.0@gmail.com>
2023-09-22 11:59:56 -0600

    search box ui modified

--- Frontend/src/components/Search.vue
+++ Frontend/src/components/Search.vue
@@ -1,13 +1,13 @@
 <template>
-  <div cl>
-    <!-- Campo de búsqueda y botón -->
-    <div class="flex items-center border rounded-full w-3/4 mx-auto mt-4 ">
-      <input v-model="term" placeholder="Search emails..." class="flex-grow py-2 px-4 outline-none rounded-l-fullb" @keyup.enter="search" />
-      <button @click="search" class="px-4 text-gray-500 hover:text-blue-400 focus:outline-none bg-white rounded-r-full">
-        <i class="fas fa-search"></i>
-      </button>
+  <form @submit.prevent="search">
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative flex justify-center">
+      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+      </div>
+      <input type="search" id="default-search " v-model="term" class="block w-3/4 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search ..." required>
+      <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
